72822649050fd949e0a6195479e1e474c9bd19bc
wangzewang <wangzewang@outlook.com>
2020-05-07 12:55:35 +0800

    Update

--- tools/dot_configs.sh
+++ tools/dot_configs.sh
@@ -4,6 +4,7 @@ declare -A mapping=(
 ["tmux.conf"]="$HOME/.tmux.conf"
 ["curlrc"]="$HOME/.curlrc"
 ["wgetrc"]="$HOME/.wgetrc"
+["zshrc"]="$HOME/.zshrc"
 ["nvim/init.vim"]="$HOME/.config/nvim/init.vim"
 ["nvim/coc-settings.json"]="$HOME/.config/nvim/coc-settings.json"
 )
